1d3b50922cda09d4281b94643a43fb6754667b90
Kayky Oliveira <kaykydeeoliveira@gmail.com>
2024-12-19 17:21:51 -0300

    Function

---
+++ Java/Lambdas/src/main/java/tests/LambdaTest02.java
@@ -0,0 +1,26 @@
+package tests;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+public class LambdaTest02 {
+
+    public static void main(String[] args) {
+        List<String> strings = List.of("Kayky ", "Oliveira ", "Guimarães");
+        List<Integer> integers = map(strings, s -> s.length());
+
+        System.out.println(integers);
+        System.out.println(map(strings, s -> s.toUpperCase()));
+    }
+
+    private static <T, R> List<R> map(List<T> list, Function<T, R> function){
+        List<R> result = new ArrayList<>();
+        for(T e : list){
+            R r = function.apply(e);
+            result.add(r);
+        }
+        return result;
+    }
+}
